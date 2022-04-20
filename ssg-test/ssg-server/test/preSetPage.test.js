712f974ca373b094b0dd9ea6f44e05ec806099e5
zhangneng01 <zhangneng01@sunlands.com>
2022-04-20 22:51:31 +0800

    feat:master

--- ssg-test/ssg-server/test/preSetPage.test.js
+++ ssg-test/ssg-server/test/preSetPage.test.js
@@ -3,12 +3,13 @@ const puppeteer = require("puppeteer");
 const {preSetPage}=require('../puppeteer');
 
 
-test('测试是否可以设置dom',()=>{
+test('测试是否可以设置dom',
+    async ()=>{
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://www.baidu.com');
     await preSetPage(page);
     const content= await page.content();
-    // 
-    escape(content).toMatch('window._useSsg = true');
+    const result=await page.evaluate(_ =>  window._useSs);
+    await expect(result).toBe(true);
 })
