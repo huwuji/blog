4795ecdafc578bcb5ed3113bebaf7699b4d31d71
zhangneng01 <zhangneng01@sunlands.com>
2022-06-19 23:01:40 +0800

    feat: fix

--- Demo/utils-test/src/components/add.js
+++
@@ -1,5 +0,0 @@
-import Decimal from "decimal.js";
-
-export default (a, b) => {
-  return Decimal(a).add(b).toNumber();
-};
--- Demo/utils-test/src/components/sub.js
+++
@@ -1,5 +0,0 @@
-import Decimal from "decimal.js";
-
-export default (a, b) => {
-  return Decimal(a).sub(b).toNumber();
-};
