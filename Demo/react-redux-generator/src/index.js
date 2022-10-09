583be899a3f5efaa80e33625a245c46e684b1a8e
zhangneng01 <zhangneng01@sunlands.com>
2022-10-09 10:38:37 +0800

    feat: react-generator  router

--- Demo/react-redux-generator/src/index.js
+++ Demo/react-redux-generator/src/index.js
@@ -1,26 +1,7 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 
-// import Routers from "./routers";
+import Routers from "./routers";
 
-// const rooter = createRoot(document.getElementById("root"));
-// rooter.render(<Routers />);
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./containers/layout";
-import Home from "./containers/home";
-import Intro from "./containers/intro/index.js";
-import NoPage from "./containers/404";
 const rooter = createRoot(document.getElementById("root"));
-rooter.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="intro" element={<Intro />} />
-        <Route path="*" element={<NoPage />} />
-      </Route>
-      <Route path="*" element={<NoPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+rooter.render(<Routers />);
--- Demo/react-redux-generator/src/routers.js
+++ Demo/react-redux-generator/src/routers.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./containers/layout";
 import Home from "./containers/home";
 import Intro from "./containers/intro/index.js";
 import NoPage from "./containers/404";
 export default () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />}>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
         <Route path="intro" element={<Intro />} />
         <Route path="*" element={<NoPage />} />
       </Route>
