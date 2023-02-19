6b609bf3792234b47fe1418393f1c5e7cee4aa71
zhangneng01 <zhangneng01@sunlands.com>
2023-02-19 15:28:24 +0800

    feat: three

--- FE_Tutorials/ThreeJs/threejs-demo/main.js
+++ FE_Tutorials/ThreeJs/threejs-demo/main.js
@@ -1,34 +1,25 @@
 import * as THREE from "three";
+import Create from "./utils/createBase.js";
+import CreateLine from "./utils/line.js";
 
-var scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-document.body.appendChild(renderer.domElement);
+const { renderer, scene, camera } = new Create();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+// camera.position.x = 5;
+// camera.position.y = 5;
 camera.position.z = 5;
+
+// 剩下的事情就是把它添加到场景中，并调用render（渲染）函数。
+const line = CreateLine(scene, renderer);
+scene.add(line);
 
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.y += 0.0;
   renderer.render(scene, camera);
 }
 animate();
-// // 兼容性处理
-// if (window.WebGL.isWebGLAvailable()) {
-//   // Initiate function or other initializations here
-//   animate();
-// } else {
-//   const warning = WebGL.getWebGLErrorMessage();
-//   document.getElementById("container").appendChild(warning);
-// }
---
+++ FE_Tutorials/ThreeJs/threejs-demo/utils/line.js
@@ -0,0 +1,29 @@
+import * as THREE from "three";
+
+/**
+ *
+ * @param {*} scene 场景
+ * @param {*} renderer 渲染器
+ */
+const createLine = function (scene, renderer) {
+  //对于线条来说，我们能使用的材质只有LineBasicMaterial 或者 LineDashedMaterial。
+  const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
+  //定义好材质之后，我们需要一个带有一些顶点的geometry（几何体）。
+  const points = [];
+  points.push(new THREE.Vector3(5, 0, 0));
+  points.push(new THREE.Vector3(0, 0, 0));
+  points.push(new THREE.Vector3(0, 10, 0));
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  //注意，线是画在每一对连续的顶点之间的，而不是在第一个顶点和最后一个顶点之间绘制线条（线条并未闭合）。
+  // 既然我们已经有了能够画两条线的点和一个材质，那我们现在就可以将他们组合在一起，形成一条线。
+  const line = new THREE.Line(geometry, material);
+  // 剩下的事情就是把它添加到场景中，并调用render（渲染）函数。
+  //   scene.add(line);
+  //   renderer.render(scene, camera);
+  return line;
+};
+
+export default createLine;
